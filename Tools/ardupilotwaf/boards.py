8768736043607bc42bd628586b7a3738aa1122be
Richard Allen <rsaxvc@gmail.com>
2023-02-09 03:55:56 +0000

    AP_HAL_Linux: Add new RSAXVC1 board

--- Tools/ardupilotwaf/boards.py
+++ Tools/ardupilotwaf/boards.py
@@ -1501,6 +1501,36 @@ class obal(linux):
             CONFIG_HAL_BOARD_SUBTYPE = 'HAL_BOARD_SUBTYPE_LINUX_OBAL_V1',
         )
 
+class rsaxvc(linux):
+    toolchain = 'arm-linux-gnueabihf'
+
+    def configure_env(self, cfg, env):
+        super(rsaxvc, self).configure_env(cfg, env)
+
+        env.CXXFLAGS += [
+            '-march=armv7-a+neon+vfpv4',
+            '-mfpu=neon-vfpv4',
+        ]
+
+        env.DEFINES.update(
+            CONFIG_HAL_BOARD_SUBTYPE = 'HAL_BOARD_SUBTYPE_LINUX_RSAXVC_V1',
+        )
+
+class rsaxvc64(linux):
+    toolchain = 'aarch64-linux-gnu'
+
+    def configure_env(self, cfg, env):
+        super(rsaxvc64, self).configure_env(cfg, env)
+
+        env.CXXFLAGS += [
+            '-march=armv8-a+simd',
+            '-mfpu=neon-fp-armv8',
+        ]
+
+        env.DEFINES.update(
+            CONFIG_HAL_BOARD_SUBTYPE = 'HAL_BOARD_SUBTYPE_LINUX_RSAXVC_V1',
+        )
+
 class SITL_static(sitl):
     def configure_env(self, cfg, env):
         super(SITL_static, self).configure_env(cfg, env)
